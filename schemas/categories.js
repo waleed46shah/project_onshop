2361e7945257635ce3f50c9e83375a08055fea79
WaleedHassan <waleed46shah@gmail.com>
2023-03-09 22:13:44 +0500

    First Commit 9/3/23

---
+++ schemas/categories.js
@@ -0,0 +1,13 @@
+export default{
+    name:'categories',
+    title:'categories',
+    type:'document',
+    fields:[
+        {
+            name:'name',
+            title:'Category Name',
+            type:'string'
+        },
+        
+    ]
+}
